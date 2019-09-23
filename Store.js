bb5651b5a0754af46795a310a36ffa360c94ea82
Kevin Davis <Kevindavis5454@gmail.com>
2019-09-23 08:48:59 -0600

    added inital framework html css js js array

---
+++ Store.js
@@ -0,0 +1,81 @@
+const STORE = {
+    questions: [
+        //1
+        {
+            question: "What country is it thought that Australian Shepherds originated from?",
+            options: [
+                "Australia",
+                "Spain",
+                "USA",
+                "Scotland"
+            ],
+            answer: "Spain"
+        },
+        //2
+        {
+            question: "What part of the body does the condition Heterochromia effect on Australian Shepherds?",
+            options: [
+                "Body",
+                "Feet",
+                "Eyes",
+                "Tail"
+            ],
+            answer: "Eyes"
+        },
+        //3
+        {
+            question: "What chance does an Australian Shepherd roughly have, to be born with a bobbed tail?",
+            options: [
+                "1 in 100",
+                "1 in 5",
+                "1 in 1000",
+                "1 in 40"
+            ],
+            answer: "1 in 5"
+        },
+        //4
+        {
+            question: "What height of objects can Australian Shepherds easily jump over?",
+            options: [
+                "7 to 8 feet",
+                "5 to 6 feet",
+                "3 to 4 feet",
+                "Up to 9 feet"
+            ],
+            answer: "3 to 4 feet"
+        },
+        //5
+        {
+            question: "How long is an average Australian Shepherds lifespan?",
+            options: [
+                "8 years",
+                "14 years",
+                "15 years",
+                "13 years"
+            ],
+            answer: "13 years"
+        },
+        //6
+        {
+            question: "Are Australian Shepherds recommended for families with young children?",
+            options: [
+                "Yes",
+                "No"
+            ],
+            answer: "Yes"
+        },
+        //7
+        {
+            question: "The same gene that causes Australian Shepherds to have multi-colored coats can also cause what health issues?",
+            options: [
+                "Blindness and/or Deafness",
+                "Heart failure",
+                "Kidney failure",
+                "Rapid tooth decay"
+            ],
+            answer: "Blindness and/or Deafness"
+        },
+    ],
+    currentQuestion: 0,
+    score: 0
+};
---
+++ main.js
@@ -0,0 +1,171 @@
+//You should be able to click a button to start the quiz
+// A question should be rendered to the DOM
+// You should be able to click a button next to the answer to select your answer
+// You should be able to submit your answer with a button and taken to the next question
+// You should be able to track how many questions you have answered correctly at the top of the page
+// You should be able to track what question you're currently on at the top of the page
+// You should be able to see your results after completing the quiz
+
+
+
+// This function will begin the quiz
+function handleQuizStart() {
+    $('#start-button').on('click', function(event) {
+        renderQuestion();
+    }
+    );
+
+    console.log('`handleQuizStart` ran');
+}
+
+// Update/display the question number and score obtained at the top of the screen
+function updateQuestionNumberAndScore() {
+    const html = $(`<ul>
+        <li id="js-answered">Questions Number: ${STORE.currentQuestion +1}/${STORE.questions.length}</li>
+        <li id="js-score">Score: ${STORE.score}/${STORE.questions.length}</li>
+      </ul>`);
+    $(".question-and-score").html(html);
+
+    console.log('`updateQuestionNumberAndScore` ran');
+}
+
+// This function displays the new answer options for the new Question
+function updateAnswerOptions() {
+    let question = STORE.questions[STORE.currentQuestion];
+    for (let i=0; i < question.options.length; i++) {
+        $('.js-options').append(`
+            <input type= "radio" name="options" id="option${i+1}" value= "${question.options[i]}" tabindex ="${i+1}"> 
+            <label for="option${i+1}"> ${question.options[i]} </label><br/>
+            <span id="js-r${i+1}"></span>
+        `);
+    }
+
+    console.log('`updateAnswerOptions` ran');
+}
+
+// This function displays the question in the DOM
+function renderQuestion() {
+    let question = STORE.questions[STORE.currentQuestion];
+    updateQuestionNumberAndScore();
+    const questionHtml = $(`
+    <div>
+        <form id="js-questions" class="question-form">
+            <fieldset>
+                <div class="js-row question">
+                    <div class="js-col-12">
+                        <legend> ${question.question}</legend>
+                    </div>
+                </div>
+                
+                <div class="js-row options">
+                    <div class="js-col-12">
+                        <div class="js-options"></div>
+                    </div>
+                </div>
+                
+                <div class="js-row">
+                    <div class="js-col-12">
+                        <button type= "submit" id="answer" tabindex="5">Submit</button>
+                        <button type= "button" id="next-question" tabindex="6">Next >></button>
+                    </div>
+                </div>
+            </fieldset>
+            </form>
+        </div>`);
+    $("main").html(questionHtml);
+    updateAnswerOptions();
+    $("#next-question").hide();
+
+    console.log('`renderQuestion` ran');
+}
+
+// This function will display the results page to the DOM
+function displayResultsPage() {
+    let resultHtml = $(`
+    <div class="results">
+        <form id="js-restart-quiz">
+            <fieldset>
+                <div class="js-row">
+                    <div class="js-col-12">
+                        <legend>Your score is: ${STORE.score}/${STORE.questions.length}</legend>
+                    </div>
+                </div>
+                
+                <div class="js-row">
+                    <div class="js-col-12">
+                        <button type="button" id="restart">Restart Quiz</button>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+        </div>`);
+    STORE.currentQuestion = 0;
+    STORE.score = 0;
+    $("main").html(resultHtml);
+
+    console.log('`displayResultsPage` ran');
+}
+
+// This function will check whether it has reached the end of the questions list
+function isThisLastQuestion() {
+    $('body').on('click', '#next-question', (event) => {
+        STORE.currentQuestion === STORE.questions.length ? displayResultsPage() : renderQuestion();
+    });
+
+    console.log('`isThisLastQuestion` ran')
+}
+
+// This function will check whether the chosen option is right or wrong
+function isTheAnswerCorrect() {
+    $('body').on("submit", '#js-questions', function(event) {
+        event.preventDefault();
+        let currentQues = STORE.questions[STORE.currentQuestion];
+        let selectedOption = $("input[name=options]:checked").val();
+        if (!selectedOption) {
+            alert("Choose an option");
+            return;
+        }
+        let id_num = currentQues.options.findIndex(i => i === selectedOption);
+        let id = "#js-r" + ++id_num;
+        $('span').removeClass("right-answer wrong-answer");
+        if(selectedOption === currentQues.answer) {
+            STORE.score++;
+            $(`${id}`).append(`You got it right<br/>`);
+            $(`${id}`).addClass("right-answer");
+        }
+        else {
+            $(`${id}`).append(`You got it wrong <br/> The answer is "${currentQues.answer}"<br/>`);
+            $(`${id}`).addClass("wrong-answer");
+        }
+
+        STORE.currentQuestion++;
+        $("#js-score").text(`Score: ${STORE.score}/${STORE.questions.length}`);
+        $('#answer').hide();
+        $("input[type=radio]").attr('disabled', true);
+        $('#next-question').show();
+    });
+
+    console.log('`isTheAnswerCorrect` ran');
+}
+
+function restartQuiz() {
+    $('body').on('click', '#restart', (event) => {
+        renderQuestion();
+    });
+
+    console.log('`restartQuiz` ran');
+}
+
+
+// This function will be the callback when the page loads. Its responsible for rendering the quiz questions
+// handling the question selecting, and question transition, as well as rendering the results page.
+function handleQuizApp() {
+
+    handleQuizStart(); //
+    isThisLastQuestion(); //
+    isTheAnswerCorrect();//
+    restartQuiz();//
+
+}
+
+$(handleQuizApp);
